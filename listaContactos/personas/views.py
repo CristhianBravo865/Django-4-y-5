042ab8f632c5e15791bc0a8c4f4d36f05f06a113
CristhianBravo865 <cristhian.bravo8657@gmail.com>
2025-06-28 17:28:58 -0500

    Implementación de formulario plano RawPersonaForm con nueva vista y rutas

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import PersonaForm, RawPersonaForm
+from django.shortcuts import render
+
+def personasAnotherCreateView(request):
+    form = RawPersonaForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'personas/personasCreate.html', context)
