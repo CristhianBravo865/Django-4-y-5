82c9b82a593b3fdfa57436eb3bcd9445eca4f5e7
CristhianBravo865 <cristhian.bravo8657@gmail.com>
2025-06-28 19:03:43 -0500

    Implementación de RawPersonaForm con manejo diferenciado de GET y POST

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render, redirect
-from .forms import PersonaForm, RawPersonaForm
-from .models import Persona  # Asegúrate de importar el modelo
+from .forms import RawPersonaForm
+from .models import Persona
 
 def personasAnotherCreateView(request):
-    form = RawPersonaForm()
-
-    if request.method == "POST":
+    if request.method == "GET":
+        form = RawPersonaForm()
+    
+    elif request.method == "POST":
         form = RawPersonaForm(request.POST)
         if form.is_valid():
-            # Se guardan los datos manualmente
             Persona.objects.create(
                 nombre=form.cleaned_data['nombre'],
                 apellido=form.cleaned_data['apellido'],
                 edad=form.cleaned_data['edad']
             )
-            return redirect('/')
-
+            return redirect('/')  
+    
     context = {
         'form': form
     }
-    return render(request, "another_add.html", context)
+    return render(request, 'another_add.html', context)
 
 def myHomeView(request):
     return render(request, "home.html", {})
